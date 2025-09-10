153f687e1daf8fe81c2248718762d15a824ca3a3
SamakshAgarwal1112 <agarwalsamaksh11@gmail.com>
2025-09-11 02:29:39 +0530

    Using arguments

--- main.cpp
+++ main.cpp
@@ -4,19 +4,31 @@
 
 using namespace std;
 
+string filename;
+
 int main(int argc, char* argv[]){
-    std::cout << "Enter file name\n";
-    string filename;
-    std::cin >> filename;
+
+    if(argc > 2) {
+        cout << "Expected 1 argument, got " << argc - 1 << " arguments.\n";
+        return 0;
+    }
+    else if(argc == 2) {
+        filename = argv[1];
+    }
+    else {
+        std::cout << "Enter file name\n";
+        getline(std::cin, filename);
+    }
+
     ifstream myfile(filename);
     if(myfile.is_open()) {
         string line;
         while (getline(myfile, line)) {
-            cout << line << "\n";
+            std::cout << line << "\n";
         }
         myfile.close();
     } else {
-        cout << "Unable to open file " << filename << ". Invalid file.\n";
+        std::cout << "Unable to open file " << filename << ".\n";
     }
     return 0;
 }
