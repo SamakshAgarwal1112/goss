cb9131070a6d13e35d9c7b2bc88a977614585e4c
SamakshAgarwal1112 <agarwalsamaksh11@gmail.com>
2025-09-13 23:30:10 +0530

    Added time evaluation for kmp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <fstream>
 #include "kmp.h"
+#include <chrono>
+#include <ctime>
 
 using namespace std;
 
@@ -24,17 +26,30 @@ int main(int argc, char* argv[]){
 
     pattern = argv[1];
     KMP kmp(pattern);
+    auto pi_fill_start = std::chrono::system_clock::now();
     kmp.fillPiTable();
+    auto pi_fill_end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> pi_fill_time = pi_fill_end - pi_fill_start;
+    auto pi_fill_time_in_ms = std::chrono::duration_cast<std::chrono::milliseconds>(pi_fill_time);
+    std::cout << "Pi Table filled in " << pi_fill_time_in_ms.count() << " milliseconds" << endl;
 
     ifstream myfile(filename);
     int i = 1;
     if(myfile.is_open()) {
         string line;
+        auto text_search_start = std::chrono::system_clock::now();
         while (getline(myfile, line)) {
-            std::cout << line << "\n";
+            // std::cout << line << "\n";
             int index = kmp.getIndex(line);
             if(index!=-1) std::cout << "Pattern found at " << index << " at line " << i++ << ".\n";
         }
+        auto text_search_end = std::chrono::system_clock::now();
+
+        std::chrono::duration<double> text_search_time = text_search_end - text_search_start;
+        auto text_search_time_in_ms = std::chrono::duration_cast<std::chrono::milliseconds>(text_search_time);
+        std::cout << "Text search filled in " << text_search_time_in_ms.count() << " milliseconds" << endl;
+
         myfile.close();
     } else {
         std::cout << "Unable to open file " << filename << ".\n";
